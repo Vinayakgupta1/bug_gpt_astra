e68b2eab9ddb4a3cabc5a1b2ff226f7babff08fb
Vinayak <158466468+Vinayakgupta1@users.noreply.github.com>
2025-03-25 17:32:22 +0530

    project

--- project/src/database/__init__.py
+++
@@ -1,8 +0,0 @@
-from .engine import async_session
-from .init_db import init_db
-from .models import Base
-
-
-async def get_session():
-    async with async_session() as session:
-        yield session
